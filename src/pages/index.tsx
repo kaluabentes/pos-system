931882f1a87187ff57d457f44bb6fc848bebacb1
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-15 21:15:21 -0300

    chore: config styleguide tooling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
-function Home() {
-  return <h1>Hello, Next.js boilerplate</h1>;
+import React from 'react'
+
+function Home(): React.ReactElement {
+  return <h1>Hello, Next.js boilerplate</h1>
 }
 
-export default Home;
+export default Home
