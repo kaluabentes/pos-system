b1ce766cc1df84aa10687edfc17e7b512d999067
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-16 21:52:20 -0300

    chore: add next optimized images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
-const withImages = require('next-images')
+const withPlugins = require('next-compose-plugins')
+const optimizedImages = require('next-optimized-images')
 
-module.exports = withImages({
-  esModule: true
-})
+module.exports = withPlugins([optimizedImages])
