3994d911f27052e82cd8823d61e72ca18a1179de
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-16 21:48:22 -0300

    feat: add index styles

---
+++ src/styles/pages/index.js
@@ -0,0 +1,26 @@
+import styled, { keyframes } from 'styled-components'
+
+import LogoImage from '../../assets/logo.svg'
+
+const blink = keyframes`
+  0% {
+    opacity: 1;
+  }
+
+  50% {
+    opacity: 0.3;
+  }
+
+  100% {
+    opacity: 1;
+  }
+`
+
+export const Logo = styled(LogoImage)`
+  fill: #fff;
+  animation-name: ${blink};
+  animation-duration: 3s;
+  animation-iteration-count: infinite;
+  animation-fill-mode: forwards;
+  animation-timing-function: ease-in;
+`
