ff3df0a73bcab757f62d1798204a5228c889a719
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-19 00:28:53 -0300

    feat: add media query util

---
+++ src/styles/utils/media.ts
@@ -0,0 +1,23 @@
+import { css, CSSObject, FlattenSimpleInterpolation } from 'styled-components'
+
+const SMALL = 767
+const MEDIUM = 768
+const LARGE = 1024
+
+export const media = {
+  sm: (styles: CSSObject): FlattenSimpleInterpolation => css`
+    @media screen and (max-width: ${SMALL}px) {
+      ${css(styles)}
+    }
+  `,
+  md: (styles: CSSObject): FlattenSimpleInterpolation => css`
+    @media screen and (max-width: ${MEDIUM}px) {
+      ${css(styles)}
+    }
+  `,
+  lg: (styles: CSSObject): FlattenSimpleInterpolation => css`
+    @media screen and (min-width: ${LARGE}px) {
+      ${css(styles)}
+    }
+  `
+}
