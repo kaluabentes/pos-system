52c6ee2c066cc175517d88187aac61f7991cff9f
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-26 00:50:35 -0300

    feat: create user endpoints

---
+++ src/pages/api/users/[id].ts
@@ -0,0 +1,44 @@
+import { NowRequest, NowResponse } from '@vercel/node'
+
+import UsersService from 'modules/users/UsersService'
+import connectDb from 'utils/connectDb'
+
+export default async function (req: NowRequest, res: NowResponse) {
+  const {
+    query: { id },
+    body,
+    method
+  } = req
+
+  await connectDb()
+
+  switch (method) {
+    case 'GET':
+      try {
+        const user = await UsersService.getOne(id)
+        res.json(user)
+      } catch (error) {
+        res.status(500).send({ message: error.message })
+      }
+      break
+    case 'PATCH':
+      try {
+        const user = await UsersService.update(id, body)
+        res.send(user)
+      } catch (error) {
+        res.status(400).send({ message: error.message })
+      }
+      break
+    case 'DELETE':
+      try {
+        const result = await UsersService.delete(id)
+        res.json(result)
+      } catch (error) {
+        res.status(500).send({ message: error.message })
+      }
+      break
+    default:
+      res.setHeader('Allow', ['GET', 'PUT', 'DELETE'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}
