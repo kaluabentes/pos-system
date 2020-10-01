5c2995d0db49d566f95f87019f5b5fb5d7830a9b
Kaluã Bentes <kaluanbentes@gmail.com>
2020-10-01 00:23:11 -0300

    refactor: add layout util

--- src/components/atoms/Paper/index.ts
+++ src/components/atoms/Paper/index.ts
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
-import { space } from 'styled-system'
+import { space, layout } from 'styled-system'
 
-export default styled.div<{ padding?: string }>`
+export default styled.div<{ padding?: string; width?: string }>`
   ${space}
+  ${layout}
   background: white;
   border-radius: 4px;
   box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.1);
